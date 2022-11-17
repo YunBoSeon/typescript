0a13648f71af23e411a9e9d08fdc8c5256004858
korDev <iamakoreandeveloper@gmail.com>
2022-11-17 22:03:17 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface treeItem {
+    title:string,
+    contents:string
+}
+
+var tree:Array<treeItem> = [
+    { title : "안녕하세요.", contents : "반갑습니다." },
+    { title : "안녕하세요2.", contents : "반갑습니다2." },
+    { title : "안녕하세요3.", contents : "반갑습니다3." },
+    { title : "안녕하세요4.", contents : "반갑습니다4." }
+];
+
+const treeFiller = async() => {
+    
+};
